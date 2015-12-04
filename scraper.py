1834ae0d35fd496decb4bfab5e9b70d241fd3130
thinker007 <thinkerisme@gmail.com>
2015-12-04 21:07:15 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -31,34 +31,47 @@ from multiprocessing import cpu_count
 import grequests
 
 start_urls = ['http://www.tvmao.com/drama/KSExaik=/actors#']
+url = 'http://www.tvmao.com/drama/KSExaik=/actors#'
+headers = {
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+        'Accept': '*/*',
+
+    }
+
 pool = Pool(cpu_count() * 20)
+
+def make_dict(table):
+    result = []
+    allrows = table.findAll('tr')
+    keys = allrows[0].findAll('td',text=True)
+    
+    for row in allrows[1:]:
+        item = {}
+        allcols = row.findAll('td')
+        for k in range(len(keys)):
+            col = allcols[k]
+            if col.find('a'):
+                a = col.find('a')
+                v = {}
+                v['@type'] = "uri"
+                v['href'] = a['href']
+                v['name'] = a.text
+                item[keys[k]] = v
+            else:
+                item[keys[k]] = col.text
+        result.append(item)
+    return result
 
 def scrape(response, **kwargs):
         listing_soup = bs(response.text, 'lxml')
-        asin_nums = listing_soup.find_all('div', 'zg_itemImmersion')
-        for asin_num in asin_nums:
-            asin = ''
-            try:
-                asin = asin_num.find('a')['href'].split('dp/')[-1].strip()
-            except:
-                pass
-            amazon_price = ''
-            try:
-                amazon_price = asin_num.find('strong', 'price').text.strip()
-            except:
-                pass
-            total_offer_count = ''
-            try:
-                total_offer_count = asin_num.find('div', 'zg_usedPrice').find('a').text.strip().split(u'\xa0')[0].replace('used & new', '')
-            except:
-                pass
-            lowest_price = ''
-            try:
-                lowest_price = asin_num.find('div', 'zg_usedPrice').find('span', 'price').text.strip()
-            except:
-                pass
-            today_date = str(datetime.now())
-            scraperwiki.sqlite.save(unique_keys=['Date'], data={'ASIN': asin, 'Date': today_date, 'Amazon Price': amazon_price, 'Total Offer Count': total_offer_count, 'Lowest Price': lowest_price})
+        table = soup.findAll('table',{'class':'tbcrew'})[0]
+        yanyuanbiao = make_dict(table)
+        
+        today_date = str(datetime.now())
+        scraperwiki.sqlite.save(unique_keys=['Date'], data={'ASIN': asin, 'Date': today_date, 'Amazon Price': amazon_price, 'Total Offer Count': total_offer_count, 'Lowest Price': lowest_price})
 
 
 def multiparse(links):
@@ -71,9 +84,8 @@ def parse(url):
     if url in start_urls:
         async_list = []
         print (url)
-        for i in xrange(1, 6):
-            rs = (grequests.get(url+'?&pg={}'.format(i), hooks = {'response' : scrape}))
-            async_list.append(rs)
+        rs = (grequests.get(url,headers=headers, hooks = {'response' : scrape}))
+        async_list.append(rs)
         grequests.map(async_list)
     page = requests.get(url)
     soup = bs(page.text, 'lxml')
@@ -103,39 +115,8 @@ if __name__ == '__main__':
 
 
 
-url = 'http://www.tvmao.com/drama/KSExaik=/actors#'
-headers = {
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-        'Accept': '*/*',
 
-    }
 
-def make_dict(table):
-    result = []
-    allrows = table.findAll('tr')
-    keys = allrows[0].findAll('td',text=True)
+
     
-    for row in allrows[1:]:
-        item = {}
-        allcols = row.findAll('td')
-        for k in range(len(keys)):
-            col = allcols[k]
-            if col.find('a'):
-                a = col.find('a')
-                v = {}
-                v['@type'] = "uri"
-                v['href'] = a['href']
-                v['name'] = a.text
-                item[keys[k]] = v
-            else:
-                item[keys[k]] = col.text
-        result.append(item)
-    return result
-    
-r = requests.get(url,headers=headers)
-soup = BeautifulSoup.BeautifulSoup(r.content)
-table = soup.findAll('table',{'class':'tbcrew'})[0]
-juese = make_dict(table)
+
