10b57db73cd04f5d6dc5ec4d8dea889298785e6f
thinker007 <thinkerisme@gmail.com>
2015-12-04 20:07:30 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,42 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+import requests
+import BeautifulSoup
+
+url = 'http://www.tvmao.com/drama/KSExaik=/actors#'
+headers = {
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+        'Accept': '*/*',
+
+    }
+
+def make_dict(table):
+    result = []
+    allrows = table.findAll('tr')
+    keys = allrows[0].findAll('td',text=True)
+    
+    for row in allrows[1:]:
+        item = {}
+        allcols = row.findAll('td')
+        for k in range(len(keys)):
+            col = allcols[k]
+            if col.find('a'):
+                a = col.find('a')
+                v = {}
+                v['@type'] = "uri"
+                v['href'] = a['href']
+                v['name'] = a.text
+                item[keys[k]] = v
+            else:
+                item[keys[k]] = col.text
+        result.append(item)
+    return result
+    
+r = requests.get(url,headers=headers)
+soup = BeautifulSoup.BeautifulSoup(r.content)
+table = soup.findAll('table',{'class':'tbcrew'})[0]
+juese = make_dict(table)
